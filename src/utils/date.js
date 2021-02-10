221d5edf15cd69aefa3d99afb1bd0a16dafffb06
Sulton <msulton55@gmail.com>
2021-02-10 22:16:02 +0700

    Adding date format on utils

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,9 @@
+import dateFormat from "dateformat"; 
+
 const date = new Date();
 
 export const GetCurrentYear = date.getFullYear();
+
+export const FormatDate = (date) => {
+  return dateFormat(date, "d mmmm yyyy")
+}
